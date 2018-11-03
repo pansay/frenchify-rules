0f6a1c06ee7e87e363bf4019c0e0a2064f18ce08
pansay <jan.krepelka@gmail.com>
2018-11-03 22:26:53 +0100

    env grr

---
+++ specs/frenchify.spec.js
@@ -0,0 +1,5 @@
+describe('Frenchify', () => {
+  it('should...', () => {
+    expect(5).toBe(5);
+  });
+});
--- src/frenchify.js
+++ src/frenchify.js
@@ -1,22 +1,21 @@
-export default class Frenchify {
+// export default class Frenchify {
+//   rulesArray = [];
 
-  rulesArray = [];
+//   constructor(rulesArray) {
+//     this.rulesArray = rulesArray;
+//   }
 
-  constructor(rulesArray) {
-    this.rulesArray = rulesArray;
-  }
+//   applyRules(content) {
+//     this.rulesArray.forEach(rules => {
+//       content = this.replace(content, rules);
+//     });
+//     return content;
+//   }
 
-  applyRules(content) {
-    this.rulesArray.forEach(rules => {
-      content = this.replace(content, rules);
-    });
-    return content;
-  }
-
-  replace(content, rules) {
-    rules.forEach(rule => {
-      content = content.replace(new RegExp(rule.from, 'gmi'), rule.to);
-    });
-    return content;
-  }
-}
+//   replace(content, rules) {
+//     rules.forEach(rule => {
+//       content = content.replace(new RegExp(rule.from, 'gmi'), rule.to);
+//     });
+//     return content;
+//   }
+// }
