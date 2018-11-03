8726517f05903c7aef9728b651802e9f927ca467
pansay <jan.krepelka@gmail.com>
2018-11-03 21:15:22 +0100

    initial setup

---
+++ src/frenchify.js
@@ -0,0 +1,22 @@
+export default class Frenchify {
+
+  rulesArray = [];
+
+  constructor(rulesArray) {
+    this.rulesArray = rulesArray;
+  }
+
+  applyRules(content) {
+    this.rulesArray.forEach(rules => {
+      content = this.replace(content, rules);
+    });
+    return content;
+  }
+
+  replace(content, rules) {
+    rules.forEach(rule => {
+      content = content.replace(new RegExp(rule.from, 'gmi'), rule.to);
+    });
+    return content;
+  }
+}
