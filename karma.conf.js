68dafd509e6ec1a500e9f22d2c7cd0ba299dbc42
pansay <jan.krepelka@gmail.com>
2018-11-03 22:56:29 +0100

    clean up

--- karma.conf.js
+++ karma.conf.js
@@ -2,12 +2,19 @@ module.exports = function(config) {
   config.set({
 
     basePath: '',
+    port: 9876,
+    autoWatch: false,
+    singleRun: true,
+    colors: true,
 
-    frameworks: ['jasmine', 'browserify'],
+    frameworks: [
+      'jasmine',
+      'browserify',
+    ],
 
     files: [
       'src/*.js',
-      'specs/*.js'
+      'specs/*.js',
     ],
 
     exclude: [
@@ -25,54 +32,18 @@ module.exports = function(config) {
       ],
     },
 
-
-    // babelPreprocessor: {
-    //   options: {
-    //     presets: ['env'],
-    //     sourceMap: 'inline'
-    //   },
-    //   filename: function (file) {
-    //     return file.originalPath.replace(/\.js$/, '.es5.js');
-    //   },
-    //   sourceFileName: function (file) {
-    //     return file.originalPath;
-    //   }
-    // },
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
+    reporters: [
+      'progress',
+    ],
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: [
       'PhantomJS',
       // 'Chrome',
       // 'Firefox',
     ],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
   })
 }
